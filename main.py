3e02b0c688b62df6269242cbff5ba8305bc95acd
Fernando <fernandosmartphone96@gmail.com>
2021-09-13 16:33:48 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Deu certo")
+#Comentários
+#Como funciona um programa em python (Interpretado, indentação)
+#Variáveis
+#Estrutura de dados
+#Estrutura de controle de fluxo
+#Estrutura de repetição
